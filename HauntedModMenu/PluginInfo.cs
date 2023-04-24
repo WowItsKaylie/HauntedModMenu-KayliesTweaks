933100982fc08612fe7f8b6d265a283dc9c831a4
Kaylie <lennon.m.craig@gmail.com>
2023-04-24 14:28:19 -0400

    Update PluginInfo.cs

--- HauntedModMenu/PluginInfo.cs
+++ HauntedModMenu/PluginInfo.cs
@@ -8,7 +8,7 @@ namespace HauntedModMenu
 	{
 		public const string GUID = "org.kaylie.gtag.hmm-kayliestweaks";
 		public const string Name = "HMM-KayliesTweaks";
-		// The first 3 numbers are inherited from upstream
+		// The first 3 numbers are inherited from upstream and the version of HMM its based on
 		// the last number is the iteration of KayliesTweaks.
 		public const string Version = "1.0.1.1";
 	}
